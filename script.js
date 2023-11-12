55b0b7d89dab2f1f1bd5401d808c1f53dd7c650e
Samuel Nkongolo <samuelnkongolo@cmail.carleton.ca>
2023-11-12 16:05:24 -0500

    test

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Data storage - Initialize the array of To Do items
 let todoItems = [];
-let unique = [0];
+let uniqueCount = [0];
 
 // Function to add a todo to the list
-function addToDoItem(text) {
-  if (typeof text === "string") {
+function addToDoItem(taskDescription) {
+  if (typeof taskDescription === "string") {
     console.log("Thanks a lot!");
-    unique[0]++;
-    let isCompleted = false;
-    let ID = unique[0];
+    uniqueCount[0]++;
+    let isTaskCompleted = false;
+    let taskID = uniqueCount[0];
 
     // Check if ID already exists, generate a new one if needed
-    while (todoItems.some(item => item.id === ID)) {
-      ID = unique[0];
+    while (todoItems.some(item => item.id === taskID)) {
+      taskID = uniqueCount[0];
     }
 
-    let Obj = { id: ID, text: text, completed: isCompleted };
-    todoItems.push(Obj);
+    let taskObj = { id: taskID, text: taskDescription, completed: isTaskCompleted };
+    todoItems.push(taskObj);
     console.log(todoItems);
   } else {
     console.log("Please input a string, my friend");
@@ -24,39 +24,39 @@ function addToDoItem(text) {
 }
 
 // Function to remove a todo from the list
-function removeToDoItem(todoId) {
-  if (Number.isInteger(todoId) && todoId > 0) {
+function removeToDoItem(taskID) {
+  if (Number.isInteger(taskID) && taskID > 0) {
     console.log("Nice choice of ID");
-    let diffArray = [];
+    let remainingTasks = [];
     let removed = false;
 
     for (let i = 0; i < todoItems.length; i++) {
-      if (todoItems[i].id === todoId) {
+      if (todoItems[i].id === taskID) {
         console.log("Successfully removed!");
         removed = true;
       } else {
-        diffArray.push(todoItems[i]);
+        remainingTasks.push(todoItems[i]);
       }
     }
     if (!removed) {
       console.log("Sorry, couldn't find the todo with that ID");
     }
 
-    todoItems = diffArray;
+    todoItems = remainingTasks;
   } else {
     console.log("Please input a valid ID, my friend");
   }
 }
 
 // Function to mark a task as completed
-function markToDoItemAsCompleted(todoId) {
-  if (Number.isInteger(todoId) && todoId > 0) {
+function markToDoItemAsCompleted(taskID) {
+  if (Number.isInteger(taskID) && taskID > 0) {
     console.log(todoItems[0].id);
 
     let found = false;
 
     for (let i = 0; i < todoItems.length; i++) {
-      if (todoItems[i].id === todoId) {
+      if (todoItems[i].id === taskID) {
         todoItems[i].completed = true;
         found = true;
         console.log("Successfully marked as completed!");
@@ -72,18 +72,18 @@ function markToDoItemAsCompleted(todoId) {
 }
 
 // Function to delete a task from the array
-function deleteToDoItem(todoId) {
-  if (Number.isInteger(todoId) && todoId > 0) {
+function deleteToDoItem(taskID) {
+  if (Number.isInteger(taskID) && taskID > 0) {
     console.log("Excellent choice of ID");
-    let goneArray = [];
+    let remainingTasks = [];
     let removed = false;
 
     for (let i = 0; i < todoItems.length; i++) {
-      if (todoItems[i].id === todoId) {
+      if (todoItems[i].id === taskID) {
         console.log("Successfully removed!");
         removed = true;
       } else {
-        goneArray.push(todoItems[i]);
+        remainingTasks.push(todoItems[i]);
       }
     }
 
@@ -91,7 +91,7 @@ function deleteToDoItem(todoId) {
       console.log("Couldn't find a todo with that ID");
     }
 
-    todoItems = goneArray;
+    todoItems = remainingTasks;
   } else {
     console.log("Please input a valid ID, my friend");
   }
@@ -99,15 +99,15 @@ function deleteToDoItem(todoId) {
 
 // Function to clear all completed tasks
 function clearCompletedTasks() {
-  let completedTasks = [];
+  let uncompletedTasks = [];
 
   for (let i = 0; i < todoItems.length; i++) {
     if (!todoItems[i].completed) {
-      completedTasks.push(todoItems[i]);
+      uncompletedTasks.push(todoItems[i]);
     }
   }
 
-  todoItems = completedTasks;
+  todoItems = uncompletedTasks;
 }
 
 addToDoItem("Complete coding assignment");
